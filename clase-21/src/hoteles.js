70c5faa0b7e147b94df5a48ec49c20712d1f72c0
Abdul Florez <abdulflorez1524@gmail.com>
2023-07-24 22:32:22 -0500

    Clase modificacion del DOM

---
+++ clase-21/src/hoteles.js
@@ -0,0 +1,5 @@
+export function consultarHoteles() {
+  return fetch(
+    "https://6256097e8646add390e01d99.mockapi.io/hotels/reservation/hotels"
+  );
+}
---
+++ clase-21/src/index.js
@@ -0,0 +1,42 @@
+//importamos la funcion con la que traemos la informacion de los hoteles
+import { consultarHoteles } from "./hoteles.js";
+
+//Accedemos a los elementos del DOM que necesitamos
+const buttonConsulta = document.getElementById("printButton");
+const main = document.querySelector("main");
+
+//Intersectamos el evento al que queremos usar como gatillo
+// el async indica que dentro de esta funcion existiran "await"
+buttonConsulta.addEventListener("click", async () => {
+  // Aqui ejecutamos la funcion que hace la peticion y le inticamos que espere
+  // las respuesta con await, el ".json()" pasa la respuesta a un
+  // Formato que podamos usar.
+  const respuesta = await consultarHoteles();
+  const data = await respuesta.json();
+  console.log(data);
+
+  // IMPORTANTE!!!
+  // Esta linea de codigo le dice a main que su contenido es VACIO
+  // para Limpiar el contenido de main y que no se impriman infinitamente
+  // los hoteles, si quieren borren esta linea y vean que pasa
+  main.innerHTML = "";
+
+  // recorremos el arreglo con las respuesta y usamos los elementos del DOM
+  // para imprimir las cards que hay en la respuesta UNA O UNA
+  data.forEach((hotel) => {
+    const cardHotel = document.createElement("div");
+    cardHotel.className = "card";
+    main.appendChild(cardHotel);
+
+    const imagenHotel = document.createElement("img");
+    imagenHotel.setAttribute("src", hotel.photo);
+    imagenHotel.setAttribute("alt", hotel.name);
+    imagenHotel.className = "hotel-image";
+    cardHotel.appendChild(imagenHotel);
+
+    const nombreHotel = document.createElement("h2");
+    nombreHotel.innerText = hotel.name;
+    nombreHotel.className = "hotel-name";
+    cardHotel.appendChild(nombreHotel);
+  });
+});
